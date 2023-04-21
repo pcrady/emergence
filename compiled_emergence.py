c2e75b641dd5bb416867f3eaa8e2640e1a544436
Peter Crady <petercrady1@gmail.com>
2023-04-21 01:40:06 -0500

    increase performance by cheating on calculations

--- compiled_emergence.py
+++ compiled_emergence.py
@@ -16,12 +16,11 @@ class Color(Enum):
 class NumericalMethod(Enum):
     euler = 0
     leapfrog = 1
-    runge_kutta = 2
 
 
 class Screen:
     screen_min: np.ndarray = np.array([0.0, 0.0], dtype=np.float32)
-    screen_max: np.ndarray = np.array([2000.0, 2000.0], dtype=np.float32)
+    screen_max: np.ndarray = np.array([1200.0, 1000.0], dtype=np.float32)
 
     def __init__(self) -> None:
         pygame.init()
@@ -37,8 +36,12 @@ class Screen:
         return self.clock.get_time()
 
     def update_fps(self):
-        fps = str(int(self.clock.get_fps()))
+        fps = 'Fps: ' + str(int(self.clock.get_fps()))
         fps_text = self.font.render(fps, False, pygame.Color("white"))
+        return fps_text
+
+    def number_of_particles(self, number_of_particles):
+        fps_text = self.font.render('Particles: ' + str(number_of_particles), False, pygame.Color("white"))
         return fps_text
 
 
@@ -54,6 +57,8 @@ spec = [
         ('color', int32),
         ('max_velocity', float32),
         ('epsilon', float32),
+        ('mult', float32),
+        ('slice', int32),
     ]
 
 @jitclass(spec)
@@ -79,6 +84,8 @@ class Particle(object):
         self.radius: float = 2.0
         self.max_velocity: float = 1000.0
         self.epsilon: float = 1e-7
+        self.mult: float = 5.0
+        self.slice: int = 10
 
     def __magnitude(self, vector: np.ndarray) -> np.float32:
         return np.float32(0.1 * (vector[0]**2 + vector[1]**2) ** 0.5 + self.epsilon)
@@ -108,11 +115,15 @@ class Particle(object):
         self.__handle_max_velocity()
 
     def __leapfrog_position(self) -> None:
-        self.position_vector = (self.position_vector + (self.velocity_vector * self.time_delta) + (0.5 * self.force_vector * self.time_delta**2)).astype(np.float32)
+        self.position_vector = (self.position_vector 
+                                + (self.velocity_vector * self.time_delta) 
+                                + (0.5 * self.force_vector * self.time_delta**2)).astype(np.float32)
         self.__handle_walls()
 
     def __leapfrog_velocity(self) -> None:
-        self.velocity_vector = (self.velocity_vector + 0.5 * (self.previous_force_vector + self.force_vector) * self.time_delta).astype(np.float32)
+        self.velocity_vector = (self.velocity_vector 
+                                + 0.5 * (self.previous_force_vector + self.force_vector) 
+                                * self.time_delta).astype(np.float32)
         self.__handle_max_velocity()
 
     def set_position(self) -> None:
@@ -130,26 +141,34 @@ class Particle(object):
             self.previous_force_vector = self.force_vector
             self.__leapfrog_velocity()
 
-    def set_total_force(self, particles: list['Particle']):
+    def set_total_force(self, particles: list['Particle']) -> None:
         self.force_vector = np.zeros(2, dtype=np.float32)
-        for particle in particles:
+        for particle in particles[:self.slice]:
             self.force_vector = self.force_vector + self.__calculate_force(particle).astype(np.float32)
-            
+
+    # TODO not working
+    def __calculate_parameters(self, other_particle: 'Particle') -> np.ndarray:
+        position_delta = self.position_vector - other_particle.position_vector
+        r = self.__magnitude(position_delta)
+        unit_vector = position_delta / r
+        return np.array([r, unit_vector], dtype=np.float32)
+             
     def __calculate_force(self, other_particle: 'Particle') -> np.ndarray:
         position_delta = self.position_vector - other_particle.position_vector
         r = self.__magnitude(position_delta)
-        unit_vector = np.array([position_delta[0] / r, position_delta[1] / r])
+        unit_vector = position_delta / r
+        
         # red
         if (self.color == 0):
             if (other_particle.color == 0):
                 return np.zeros(2, dtype=np.float32)
             elif (other_particle.color == 1):
                 if r > 5 * self.radius:
-                    return -unit_vector * r
+                    return -unit_vector * r * self.mult
                 else:
-                    return unit_vector * r**2
+                    return unit_vector * r**2 * self.mult
             elif (other_particle.color == 2):
-                return unit_vector**2
+                return unit_vector**2 * self.mult
         # green
         if (self.color == 1):
             if (other_particle.color == 0):
@@ -160,31 +179,31 @@ class Particle(object):
                 return np.zeros(2, dtype=np.float32)
             else:
                 if r > 10 * self.radius:
-                    return -unit_vector * r
+                    return -unit_vector * r * self.mult
                 else:
-                    return -unit_vector * r**3
+                    return -unit_vector * r**3 * self.mult
         # blue
         if (self.color == 2):
             if (other_particle.color == 0):
                 return np.zeros(2, dtype=np.float32)
             elif (other_particle.color == 1):
                 if r > 5 * self.radius:
-                    return -unit_vector * r
+                    return -unit_vector * r * self.mult
                 else:
-                    return unit_vector * r**3
+                    return unit_vector * r**3 * self.mult
             elif (other_particle.color == 2):
                 return np.zeros(2, dtype=np.float32)
             else:
-                return -unit_vector * r
+                return -unit_vector * r * self.mult
         # magenta
         if (self.color == 3):
             if (other_particle.color == 2):
                 return np.zeros(2, dtype=np.float32)
             elif (other_particle.color == 1):
                 if r > 10 * self.radius:
-                    return -unit_vector * r
+                    return -unit_vector * r * self.mult
                 else:
-                    return unit_vector * r**3
+                    return unit_vector * r**3 * self.mult
             else:
                 return np.zeros(2, dtype=np.float32)
  
@@ -193,16 +212,18 @@ class Particle(object):
 
 def draw(position_vector: np.ndarray, color: int, screen: Screen, radius) -> None:
     position_tuple = (float(position_vector[0]), float(position_vector[1]))
-    if (color == 0):
-        value = Color.red
-    elif (color == 1):
-        value = Color.green
-    elif (color == 2):
-        value = Color.blue
-    else:
-        value = Color.magenta
+    end_tuple = (float(position_vector[0]), float(position_vector[1] - 1.0))
 
-    pygame.draw.circle(screen.surface, value.value, position_tuple, radius)
+    if (color == 0):
+        colorValue = Color.red
+    elif (color == 1):
+        colorValue = Color.green
+    elif (color == 2):
+        colorValue = Color.blue
+    else:
+        colorValue = Color.magenta
+
+    pygame.draw.line(screen.surface, colorValue.value, position_tuple, end_tuple, int(radius))
 
 def draw_particles(particles: list[Particle], screen: Screen) -> None:
     for particle in particles:
@@ -219,7 +240,7 @@ def main():
     screen = Screen()    
     time_delta = 0.005
     method = NumericalMethod.leapfrog
-    num_particles = 300
+    num_particles = 1500
 
     particles = [Particle(
         screen_max = screen.screen_max,
@@ -232,7 +253,8 @@ def main():
     running = True
     while running:
         screen.fill()
-        screen.surface.blit(screen.update_fps(), (10,0))
+        screen.surface.blit(screen.update_fps(), (10, 25))
+        screen.surface.blit(screen.number_of_particles(num_particles), (10, 0))
 
         perform_computations(typed.List(particles))
         draw_particles(particles, screen)
